5747cf65832d792b3dbfa1d9fd86cfc875c37b7b
Henriqueee12 <henriquesouzalveshen@gmail.com>
2025-09-11 20:33:47 -0300

    henrique

--- freeglut/will 3d/main.cpp
+++ freeglut/will 3d/main.cpp
@@ -1,6 +1,27 @@
 #include <GL/glut.h>
 #include <stdlib.h>
 
+GLfloat escala = 0.0f;
+void teclado (unsigned char tecla,GLint x, GLint y)
+{
+
+    switch(tecla)
+    {
+    case '+' :
+        //escala ++;
+        escala=escala+0.05f;
+        break;
+    case '-' :
+        //escala --;
+         escala=escala-0.05f;
+        break;
+    }
+
+    glutPostRedisplay();
+
+
+}
+
 static void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -11,6 +32,9 @@ static void display(void)
     glLoadIdentity();
     //define área de enquadramento da cena
     gluOrtho2D(-3, 3, -3, 3);
+    glTranslatef(-2.0f, 2.0f, 0.0f);
+
+    glScalef(escala, escala, 0);
 
     //define a matrix de transformação dos modelos
     glMatrixMode(GL_MODELVIEW);
@@ -19,21 +43,19 @@ static void display(void)
 
     glLineWidth(10);
     glBegin(GL_LINES);
-        glVertex2f(-2.0f, 0.0f);
-        glVertex2f(2.0f, 0.0f);
+    glVertex2f(-2.0f, 0.0f);
+    glVertex2f(2.0f, 0.0f);
     glEnd();
 
     glBegin(GL_TRIANGLES);
-        glColor3f(1,1,0);
-        glVertex2f(-2.0f,-2.0f);
-        glVertex2f(2.0f,-2.0f);
-        glColor3f(5,3,9);
-        glVertex2f(0.0f,2.0f);
+    glColor3f(1,1,0);
+    glVertex2f(-2.0f,-2.0f);
+    glVertex2f(2.0f,-2.0f);
+    glColor3f(5,3,9);
+    glVertex2f(0.0f,2.0f);
     glEnd();
 
     glFlush();
-
-
 }
 
 int main(int argc, char *argv[])
@@ -44,6 +66,7 @@ int main(int argc, char *argv[])
     //as letras maiuscula sao constantes
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
     glutCreateWindow("GLUT Shapes");
+    glutKeyboardFunc(teclado);
     glClearColor(0.4, 0, 1, 0);
     glutDisplayFunc(display);
     glutMainLoop();
